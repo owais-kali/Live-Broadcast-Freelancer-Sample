8b611861de5a8220091035282242b5e60ec5a39d
owais-kali <sh.owais66@gmail.com>
2023-03-31 20:05:48 +0530

    Fix Elimination Loop

--- Vmix-Handler/app.js
+++ Vmix-Handler/app.js
@@ -7,10 +7,32 @@ const port = 3000;
 const vmix_url = "127.0.0.1:8088";
 const pcob_url = "127.0.0.1:10086";
 
+var playerInfoMap = new Map();
+
 // Get TotalPlayerList JSON data from PCOB
 function GetTotalPlayerList() {
   return axios.get("http://" + pcob_url + "/gettotalplayerlist");
 }
+
+// Load all players in playerInfoMap
+GetTotalPlayerList().then(
+  (obj) => {
+    var playerInfoList = obj.data.playerInfoList;
+    
+    for (let i = 0; i < playerInfoList.length; i++) {
+      const element = playerInfoList[i];
+
+      playerInfoMap.set(element.uId, {
+        Eliminated : false,
+      })
+
+    }
+  },
+  (err) => {
+    console.log(err);
+    res.status(404).send("Oh uh, something went wrong");
+  }
+);
 
 // Process TotalPlayerList
 function ProcessTotalPlayerList() {
@@ -22,10 +44,24 @@ function ProcessTotalPlayerList() {
         
         // Check LiveState
         const liveState = element.liveState;
-        if(liveState==5){
+        if(liveState==5 && playerInfoMap.get(element.uId).Eliminated == false){
+          playerInfoMap.get(element.uId).Eliminated = true;
           Elimination(element.playerName);
         }
-      }
+
+
+        // //Check Dead count
+        // const deadcount = element.killNumByGrenade;
+        // for(i=0 ; i<= deadcount;i++){
+        //   console.count(Grenadier(element.playerName));
+        // }
+
+        // //Team 
+        // const team = element.Uid;
+        // console.count(MatchIntro(element.team));
+
+
+      } 
     },
     (err) => {
       console.log(err);
@@ -51,13 +87,13 @@ app.get("/ExampleGetTotalPlayerList", (req, res) => {
 function Elimination(playerName){
   console.log("player: "+JSON.stringify(playerName)+" is dead!");
   try {
-    axios.get("http://" + vmix_url + "/api/?Function=SetText&Input=elimination.gtzip&SelectedName=RANK.Text&Value="+playerName).then((res)=>{
+    axios.get("http://" + vmix_url + "/api/?Function=SetText&Input=elimination2.gtzip&SelectedName=RANK.Text&Value="+playerName).then((res)=>{
       console.log(res);
     }, (err)=>{
 
     })
 
-    axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=elimination.gtzip");
+    // axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=elimination.gtzip");
   } catch (error) {
     console.error(error);
   }
@@ -78,8 +114,75 @@ app.get("/Fade", (req, res) => {
     console.error(error);
   }
 });
+//Domination
+function Domination(playerName){
+  console.log("player: "+JSON.stringify(playerName)+" is dead!");
+  try {
+    axios.get("http://" + vmix_url + "/api/?Function=SetText&Input=domination.gtzip&SelectedName=RANK.Text&Value="+playerName).then((res)=>{
+      console.log(res);
+    }, (err)=>{
+
+    })
+
+    axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=domination.gtzip");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+setInterval(()=>{
+  ProcessTotalPlayerList();
+}, 1000)
+
+app.get("/Test_API", (req, res) => {    
+
+})
+
+app.get("/Fade", (req, res) => {
+  try {
+    res.send(axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=1"));
+  } catch (error) {
+    console.error(error);
+  }
+});
+//Granadier
+function Grenadier(playerName){
+   console.log("player: "+JSON.stringify(playerName)+" throwed the Bomb!");
+   try {
+    axios.get("http://" + vmix_url + "/api/?Function=SetText&Input=grenadier.gtzip"+playerName).then((res)=>{
+      console.log(res);
+    }, (err)=>{
+
+    })
+
+    axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=grenadier.gtzip");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function AirDropLooted(playerName){
+  console.log("player: "+JSON.stringify(playerName)+" Air Drop Looted!");
+  try {
+   axios.get("http://" + vmix_url + "/api/?Function=SetText&Input=air drop looted.gtzip"+playerName).then((res)=>{
+     console.log(res);
+   }, (err)=>{
+
+   })
+
+   axios.get("http://" + vmix_url + "/api/?Function=Fade&Input=air drop looted.gtzip");
+ } catch (error) {
+   console.error(error);
+ }
+}
+//match intro 
+function MatchIntro(team){
+  GetTotalPlayerList();{
+   console.log(JSON.stringify())  //incomplete
+
+  }
+}
 
 app.listen(port, () => {
   console.log(`VMix Handler listening on port ${port}`);
 });
- 

7b5c47ff01c8f010449d0239bc69fa7f8486cc2a
owais-kali <sh.owais66@gmail.com>
2023-04-29 23:31:55 +0530

    Add Env

--- Main/src/Vmix/Vmix-Handler.ts
+++ Main/src/Vmix/Vmix-Handler.ts
@@ -1,11 +1,28 @@
 import axios, { AxiosResponse } from "axios";
 import { playerInfo } from "../types/playerInfo";
+import { PCOB_Handler } from "../PCOB/PCOB-Handler";
+import { getConfig } from "../env";
+
+import fs from "fs";
 
 export class VMix_Handler {
   url: string;
 
   constructor(vmix_url: string) {
     this.url = vmix_url;
+  }
+
+  SetCallbacks(PCOB_Handler: PCOB_Handler) {
+    PCOB_Handler.OnEliminated = this.Elimination.bind(this);
+  }
+
+  LoadGTs() {
+    const GTZIP_INGAME_PATH = getConfig().GTZIP_INGAME_PATH;
+
+    fs.readdirSync(GTZIP_INGAME_PATH).forEach((file) => {
+      //Title|d:\Freelancer\ESports\GTs\GTZIP_INGAME\DOMINATION.gtzip
+      if (file == "ELIMINATION.gtzip") console.log("file: " + file);
+    });
   }
 
   Elimination(playerInfo: playerInfo) {
--- Main/src/app.ts
+++ Main/src/app.ts
@@ -1,18 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 import { VMix_Handler } from "./Vmix/Vmix-Handler";
 import { PCOB_Handler } from "./PCOB/PCOB-Handler";
+import { LoadENV, getConfig } from "./env";
+
+LoadENV();
 
 const app: Express = express();
 const port = 3000;
 
-const vmix_url = "127.0.0.1:8088";
-const pcob_url = "127.0.0.1:10086";
+const vmix_url = getConfig().VMIX_URL;
+const pcob_url = getConfig().PCOB_URL;
 
 let vmix_handler = new VMix_Handler(vmix_url);
 let pcob_handler = new PCOB_Handler(pcob_url);
 
-pcob_handler.OnEliminated = vmix_handler.Elimination.bind(vmix_handler);
+vmix_handler.SetCallbacks(pcob_handler);
+vmix_handler.LoadGTs();
 pcob_handler.Start();
 
 app.listen(port, () => {

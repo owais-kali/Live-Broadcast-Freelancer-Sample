2d2e972fd5494c35d14b919603dcd7cbcc2d050d
owais-kali <sh.owais66@gmail.com>
2023-05-19 03:52:36 +0530

    Update

--- EC-Agent/src/providers/Routes.ts
+++ EC-Agent/src/providers/Routes.ts
@@ -9,14 +9,14 @@ import Log from "../middlewares/Log";
 class Routes {
   public mountPCOB_API(_express: Application): Application {
     const apiPrefix = Locals.config().PCOB_API_Prefix;
-    Log.info("Routes :: Mounting API Routes...");
+    Log.info("Routes :: Mounting Dummy PCOB API Routes...");
 
     return _express.use(`/${apiPrefix}`, PCOB_API_Router);
   }
 
   public mountVmixProxy(_express: Application): Application {
     const proxyPrefix = Locals.config().proxyPrefix;
-    Log.info("Routes :: Mounting VmixProxy Routes...");
+    Log.info("Routes :: Mounting Vmix Proxy Routes...");
 
     return _express.use(`/${proxyPrefix}`, VmixProxyRouter);
   }

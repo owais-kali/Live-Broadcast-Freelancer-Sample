3250e0621c384f29d9acaf09222f15cf9b0455b8
owais-kali <sh.owais66@gmail.com>
2023-05-26 00:20:14 +0530

    Update

--- Client/src/views/Teams/index.js
+++ Client/src/views/Teams/index.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import TeamsList from "./TeamsList"
+import { CFooter } from '@coreui/react'
+import { CButton, CCard, CCardBody, CCardHeader, CCol, CRow } from '@coreui/react'
 
 const index = () => {
     return (
         <div>
             <TeamsList />
+            <CFooter>
+                <div>
+                {['normal'].map((state, index) => (
+                <CRow className="align-items-center mb-3" key={index}>
+                  <CCol xs>
+                      <CButton
+                        color="primary"
+                        variant="outline"
+                        key={index}
+                        active={state === 'active'}
+                        disabled={state === 'disabled'}
+                      >
+                        ADD TEAM
+                      </CButton>
+                  </CCol>
+                </CRow>
+              ))}
+                </div>
+            </CFooter>
         </div>
     )
 }
